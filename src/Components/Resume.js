a3d706e63504464881d4545edd3f537ae08cbe3d
garedb <garedbarney@gmail.com>
2020-09-26 13:07:59 -0700

    moved sections around, completely overhauled skills section, added new resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,17 +5,17 @@ class Resume extends Component {
 
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
-      })
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
-        </div>
-      })
+      // var education = this.props.data.education.map(function(education){
+      //   return <div key={education.school}><h3>{education.school}</h3>
+      //   <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+      //   <p>{education.description}</p></div>
+      // })
+      // var work = this.props.data.work.map(function(work){
+      //   return <div key={work.company}><h3>{work.company}</h3>
+      //       <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+      //       <p>{work.description}</p>
+      //   </div>
+      // })
       var skills = this.props.data.skills.map(function(skills){
         var className = 'bar-expand '+skills.name.toLowerCase();
         return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
@@ -25,7 +25,7 @@ class Resume extends Component {
     return (
       <section id="resume">
 
-      <div className="row education">
+{/*      <div className="row education">
          <div className="three columns header-col">
             <h1><span>Education</span></h1>
          </div>
@@ -37,10 +37,10 @@ class Resume extends Component {
                </div>
             </div>
          </div>
-      </div>
+      </div>*/}
 
 
-      <div className="row work">
+{/*      <div className="row work">
 
          <div className="three columns header-col">
             <h1><span>Work</span></h1>
@@ -49,11 +49,11 @@ class Resume extends Component {
          <div className="nine columns main-col">
           {work}
         </div>
-    </div>
+    </div>*/}
 
 
 
-      <div className="row skill">
+     <div className="row skill">
 
          <div className="three columns header-col">
             <h1><span>Skills</span></h1>
@@ -61,14 +61,39 @@ class Resume extends Component {
 
          <div className="nine columns main-col">
 
-            <p>{skillmessage}
+           <p>{skillmessage}
             </p>
-
-				<div className="bars">
+         <div class="flex-container">
+           <div>
+            <img src = "../images/html-js.png" alt = "html logo"/>
+           </div>
+           <div>
+            <img src = "../images/node.js_logo.svg" alt = "node logo"/>
+           </div>
+           <div>
+            <img src = "../images/react.png" alt = "react logo"/>
+           </div>
+           <div>
+            <img src = "../images/c++.png" alt = "c++ logo"/>
+           </div>
+           <div>
+            <img src = "../images/unreal.png" alt = "unreal logo"/>
+           </div>
+           <div>
+            <img src = "../images/phaser.png" alt = "phaser logo"/>
+           </div>
+           <div>
+            <img src = "../images/mongodb.png" alt = "mongodb logo"/>
+           </div>
+           <div>
+            <img src = "../images/postgreSQL.png" alt = "postgreSQL logo"/>
+           </div>
+        </div>
+{/*				<div className="bars">
 				   <ul className="skills">
 					  {skills}
 					</ul>
-				</div>
+				</div>*/}
 			</div>
       </div>
    </section>
